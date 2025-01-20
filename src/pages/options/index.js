0cbcdcd065a1d1aad73c78b7b27e7e0e98d5795d
juliangrigera <juliangrigera@gmail.com>
2025-01-20 15:42:04 +0000

    Put back seat selection for V2

--- src/pages/options/index.js
+++ src/pages/options/index.js
@@ -23,6 +23,11 @@ import Footer from "../../components/Footer";
 const MapContainer = dynamic(() => import("react-leaflet").then((mod) => mod.MapContainer), { ssr: false });
 const TileLayer = dynamic(() => import("react-leaflet").then((mod) => mod.TileLayer), { ssr: false });
 const Markers = dynamic(() => import("../../components/Markers"), { ssr: false });
+const seatStructure = {
+  Back: [{row:"9-12", col:"A-C"}, {row:"9-12", col:"D-F"}, {row:"9-12", col:"G-I"}],
+  Middle: [{row:"5-8", col:"A-C"}, {row:"5-8", col:"D-F"}, {row:"5-8", col:"G-I"}],
+  Front: [{row:"1-4", col:"A-C"}, {row:"1-4", col:"D-F"}, {row:"1-4", col:"G-I"}]
+};
 
 export default function SelectOptions() {
   const router = useRouter();
@@ -89,7 +94,7 @@ export default function SelectOptions() {
         </Box>
 
         {/* Selección de hora */}
-        <Typography variant="h6" style={{ marginBottom: "6px" }}>Date</Typography>
+        <Typography variant="h6" style={{ marginBottom: "6px" }}>Preferred Time</Typography>
         <Box style={{ display: "flex", gap: "10px", marginBottom: "20px" }}>
            {timespans.map((timespan) => (
               <Button
@@ -103,27 +108,100 @@ export default function SelectOptions() {
            ))}
         </Box>
 
-        {/* Selección de distancia */}
-        <Typography variant="h6" style={{ marginBottom: "6px" }}>Distance from Home</Typography>
-        <Box style={{ display: "flex", gap: "10px", marginBottom: "20px" }}>
-          {[1, 3, 5, 10].map((distance) => (
-            <Button
-              key={distance}
-              variant={maxDistance === distance ? "contained" : "outlined"}
-              color={maxDistance === distance ? "primary" : "default"}
-              onClick={() => handleDistanceChange(distance)}
-            >
-              &lt; {distance} km
-            </Button>
+
+        <Grid container spacing={3}>
+        <Grid item xs={12} sm={6} md={6}>
+          {/* Selección de distancia */}
+          <Typography variant="h6" style={{ marginBottom: "6px" }}>Distance from Home</Typography>
+          <Box style={{ display: "flex", gap: "10px", marginBottom: "20px" }}>
+            {[1, 3, 5, 10].map((distance) => (
+              <Button
+                key={distance}
+                variant={maxDistance === distance ? "contained" : "outlined"}
+                color={maxDistance === distance ? "primary" : "default"}
+                onClick={() => handleDistanceChange(distance)}
+              >
+                &lt; {distance} km
+              </Button>
+            ))}
+          </Box>
+          <MapContainer center={[55.6761, 12.5683]} zoom={13} scrollWheelZoom={false} style={{ height: 300, width: 500 }}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Markers data={filteredTheatres} theatreSetter={setSelectedTheatre} />
+          </MapContainer>
+          
+        </Grid>
+        
+        
+        <Grid item xs={12} sm={6} md={6}>
+          <Typography variant="h6" style={{ marginBottom: "20px" }}>
+           Seat Preference
+          </Typography>
+          {/* Grilla de bloques */}
+          <Box style={{ display: "grid", gridTemplateRows: "repeat(3, auto)", gap: "20px", justifyContent: "left" }}>
+           {/* Grilla de letras de columnas */}
+           <Box style={{ display: "grid", gridTemplateColumns: "repeat(3, auto)", justifyItems: "center", marginLeft:"50px" }}>
+              {["A - C", "D - F", "G - I"].map((col) => (
+                <Typography key={col} style={{ fontWeight: "bold" }}>
+                   {col}
+                </Typography>
+              ))}
+           </Box>
+           
+           {Object.values(seatStructure).map((section, sectionIndex) => (
+              <Box key={sectionIndex} style={{ display: "grid", gridTemplateColumns: "repeat(4, auto)", gap: "20px" }}>
+                {section.map((area, areaIndex)=>(
+                  <>
+                  {areaIndex === 0 && (
+                      <Typography style={{ width: "40px", textAlign: "right", marginRight: "0px" }}>
+                        {area.row}
+                      </Typography>
+                   )}
+                   <Box style={{ display: "flex", justifyContent: "center", gap: "5px" }}>
+                       <Button
+                          key={area.row}
+                          style={{
+                            width: "40px",
+                            height: "40px",
+                            backgroundColor: (selectedSeatArea === area)
+                             ? "#1976d2" // Seleccionado
+                             : "white", // Disponible
+                            border: "1px solid black",
+                          }}
+                          onClick={() => setSelectedSeatArea(area)}
+                       />
+                    
+                    </Box>
+                  </>
+              ))}
+              </Box>
           ))}
-        </Box>
-        <MapContainer center={[55.6761, 12.5683]} zoom={13} scrollWheelZoom={false} style={{ height: 300, width: 500 }}>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Markers data={filteredTheatres} theatreSetter={setSelectedTheatre} />
-        </MapContainer>
+           {/* Indicador de pantalla */}
+           <Box
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#ccc",
+                height: "40px",
+                width: "100%",
+                maxWidth: "250px",
+                margin: "10px auto auto 50px",
+                borderRadius: "4px",
+              }}
+           >
+              <Typography style={{ fontWeight: "bold" }}>Screen</Typography>
+           </Box>
+          </Box>
+        
+        </Grid>  
+          
+        </Grid>
+
+
 
         {/* Botones de navegación */}
         <NavigationButtons onNext={handleNext} nextDisabled={!filtersSet()} onBack={handleBack} />
